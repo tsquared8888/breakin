f50fa53d861abd3725208561f14b645168ed0362
neetnectar <100286082+tsquared8888@users.noreply.github.com>
2024-11-17 22:04:30 -0800

    limit paddle and add ball
    - Paddle can no longer go off the screen
    - Created basic ball object, still needs collisions

--- breakin.js
+++ breakin.js
@@ -5,19 +5,33 @@ const GAME_HEIGHT = 256;
 const PADDLE_WIDTH = 40;
 const PADDLE_HEIGHT = 8;
 const PADDLE_SPEED = 20;
+const BALL_WIDTH = 8;
+const BALL_HEIGHT = 8;
+const BALL_SPEED = 30;
 var gameStart = false;
 var lastTime = 0;
 var gameOver = false;
 var score = 0;
+
+/* Game objects */
 var paddle = {
     x: GAME_WIDTH/2 - PADDLE_WIDTH/2,
     y: GAME_HEIGHT - PADDLE_HEIGHT,
     w: PADDLE_WIDTH,
     h: PADDLE_HEIGHT,
-    speed: 0,
+    x_vel: 0,
     movL: 0,
     movR: 0
 };
+
+var ball = {
+    x: GAME_WIDTH/2 - BALL_WIDTH/2,
+    y: GAME_HEIGHT/2 - BALL_HEIGHT/2,
+    w: BALL_WIDTH,
+    h:BALL_HEIGHT,
+    x_vel: 0,
+    y_vel: 1
+}
 
 /************* SUPPORT FUNCTIONS *************/
 
@@ -49,9 +63,17 @@ function input() {
 }
 
 function update(deltaTime) {
-    paddle.speed = 0;
-    paddle.speed = paddle.movR + paddle.movL;
-    paddle.x += paddle.speed * deltaTime;
+    paddle.x_vel = 0;
+    paddle.x_vel = paddle.movR + paddle.movL;
+    if (paddle.x >= GAME_WIDTH - paddle.w/2 && paddle.x_vel > 0 ||
+        paddle.x <= -paddle.w/2 && paddle.x_vel < 0) 
+    {
+        paddle.x = paddle.x;
+    } else {
+        paddle.x += paddle.x_vel * deltaTime;
+    }
+    ball.x += ball.x_vel * deltaTime * BALL_SPEED;
+    ball.y += ball.y_vel * deltaTime * BALL_SPEED;
 
 }
 
@@ -61,6 +83,7 @@ function draw(deltaTime) {
     ctx.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     ctx.fillStyle = 'white'
     ctx.fillRect(paddle.x, paddle.y, paddle.w, paddle.h);
+    ctx.fillRect(ball.x, ball.y, ball.w, ball.h);
 }
 
 function gameLoop(timestamp) {
